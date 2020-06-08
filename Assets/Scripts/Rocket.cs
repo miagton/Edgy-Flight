297bffce8e54b33a9f3d04b214da68fa601d2341
Miagkostup_Igor <justbl0w1991@gmail.com>
2020-06-08 21:38:52 +0300

    Added sounds + controls

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,52 @@
+﻿
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    [SerializeField] float speed = 28f;
+
+    AudioSource audioSource;
+    Rigidbody rigidBody;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    
+    void Update()
+    {
+        CheckInput();
+    }
+
+    private void CheckInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up * speed);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+
+            }
+        }
+        else 
+        {
+            audioSource.Stop();
+        }
+       // else audioSource.Stop();
+        
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            float input = Input.GetAxis("Horizontal");
+            ShipRotation(input);
+        }
+        
+    }
+
+    void ShipRotation(float multiplier)
+    {
+        transform.Rotate(-Vector3.forward*multiplier);
+    }
+}
